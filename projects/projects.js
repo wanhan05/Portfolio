e16d5b476851ddf2e046eeb566d8010a5fda5ff5
Wanhan Sun <wanhansun22@gmail.com>
2026-04-30 15:15:13 -0700

    lab 5 test

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -6,4 +7,94 @@ const projectsContainer = document.querySelector('.projects');
 document.querySelector('.projects-title').textContent =
   `Projects (${projects.length})`;
 
+let query = '';
+let selectedIndex = -1;
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+function renderPieChart(projectsGiven) {
+  let rolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+
+  let data = rolledData.map(([year, count]) => ({
+    value: count,
+    label: year,
+  }));
+
+  let sliceGenerator = d3.pie().value((d) => d.value);
+  let arcData = sliceGenerator(data);
+  let arcs = arcData.map((d) => arcGenerator(d));
+
+  // Clear svg and legend
+  let svg = d3.select('svg');
+  svg.selectAll('path').remove();
+  let legend = d3.select('.legend');
+  legend.selectAll('li').remove();
+
+  // Draw pie slices
+  arcs.forEach((arc, i) => {
+    svg
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(i))
+      .attr('class', selectedIndex === i ? 'selected' : '')
+      .on('click', () => {
+        selectedIndex = selectedIndex === i ? -1 : i;
+
+        svg.selectAll('path').attr('class', (_, idx) =>
+          idx === selectedIndex ? 'selected' : ''
+        );
+
+        legend.selectAll('li').attr('class', (_, idx) =>
+          idx === selectedIndex ? 'legend-item selected' : 'legend-item'
+        );
+
+        // Filter projects by selected wedge
+        let filteredProjects = getFilteredProjects();
+        if (selectedIndex === -1) {
+          renderProjects(filteredProjects, projectsContainer, 'h2');
+        } else {
+          let selectedYear = data[selectedIndex].label;
+          renderProjects(
+            filteredProjects.filter((p) => p.year === selectedYear),
+            projectsContainer,
+            'h2'
+          );
+        }
+      });
+  });
+
+  // Draw legend
+  data.forEach((d, idx) => {
+    legend
+      .append('li')
+      .attr('class', idx === selectedIndex ? 'legend-item selected' : 'legend-item')
+      .attr('style', `--color:${colors(idx)}`)
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+  });
+}
+
+function getFilteredProjects() {
+  return projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+}
+
+// Initial render
 renderProjects(projects, projectsContainer, 'h2');
+renderPieChart(projects);
+
+// Search
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('input', (event) => {
+  query = event.target.value;
+  selectedIndex = -1;
+  let filteredProjects = getFilteredProjects();
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
+});
